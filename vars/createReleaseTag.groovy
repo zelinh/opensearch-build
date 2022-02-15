af183bfbea45b5bdbe598524eaaa807da31ffff2
Zelin Hao <zelinhao@amazon.com>
2022-02-14 17:41:20 -0800

    Change the path
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

---
+++ vars/createReleaseTag.groovy
@@ -0,0 +1,18 @@
+def call(Map args = [:]) {
+
+    def lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
+    def buildManifestObj = lib.jenkins.BuildManifest.new(readYaml(file: args.buildManifest))
+
+    String opensearchCommitId = buildManifestObj.getCommitId()
+    echo "Commit ID for OpenSearch is $opensearchCommitId"
+
+    def componentsName = buildManifestObj.getComponets()
+    def componetsNumber = componentsName.size()
+    def componets1 = componentsName.get(1)
+    echo "There are $componetsNumber components in ths manifest"
+    echo "The second component is called $componets1"
+    def componets2 = componentsName.get(2)
+    echo "The third component is called $componets2"
+
+    return buildManifestObj
+}
