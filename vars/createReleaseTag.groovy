dd8a74839f2ef79abb67cf295798a4b29a93fc34
Zelin Hao <zelinhao@amazon.com>
2022-02-15 17:19:58 -0800

    Commit
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/createReleaseTag.groovy
+++ vars/createReleaseTag.groovy
@@ -17,12 +17,24 @@ def call(Map args = [:]) {
         pwd
         ls $WORKSPACE/builds/opensearch
     """
-    for (component in componentsName) {
-        echo "The component name is $component"
-        def commitID = buildManifestObj.getCommitId(component)
-        echo "The commit ID for $component is $commitID"
-        def repo = buildManifestObj.getRepo(component)
-        echo "The URL for $component is $repo"
+    withCredentials([usernamePassword(credentialsId: "${GITHUB_BOT_TOKEN_NAME}", usernameVariable: 'GITHUB_USER', passwordVariable: 'GITHUB_TOKEN')]) {
+        for (component in componentsName) {
+            echo "The component name is $component"
+            def commitID = buildManifestObj.getCommitId(component)
+            echo "The commit ID for $component is $commitID"
+            def repo = buildManifestObj.getRepo(component)
+            echo "The URL for $component is $repo"
 
+        }
+        sh """
+            #!/bin/bash
+            set +x
+            export ROLE=${SIGNER_CLIENT_ROLE}
+            export EXTERNAL_ID=${SIGNER_CLIENT_EXTERNAL_ID}
+            export UNSIGNED_BUCKET=${SIGNER_CLIENT_UNSIGNED_BUCKET}
+            export SIGNED_BUCKET=${SIGNER_CLIENT_SIGNED_BUCKET}
+            $WORKSPACE/sign.sh ${arguments}
+        """
     }
+
 }
