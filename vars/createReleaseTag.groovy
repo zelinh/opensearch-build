48e44335ba90b52ffd8b2f8258093714a1e78ec5
Zelin Hao <zelinhao@amazon.com>
2022-02-22 12:00:14 -0800

    Test tag
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/createReleaseTag.groovy
+++ vars/createReleaseTag.groovy
@@ -1,8 +1,8 @@
 def call(Map args = [:]) {
 
     def lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
-    def buildManifestObj = lib.jenkins.BuildManifest.new(readYaml(file: args.buildManifest))
-
+    //def buildManifestObj = lib.jenkins.BuildManifest.new(readYaml(file: args.buildManifest))
+    def buildManifestObj = lib.jenkins.BuildManifest.new(readYaml(file: "opensearch-build/jenkins/release-tag/manifest.yml"))
     String opensearchCommitId = buildManifestObj.getCommitId("OpenSearch")
     echo "Commit ID for OpenSearch is $opensearchCommitId"
 
@@ -10,43 +10,49 @@ def call(Map args = [:]) {
     def componetsNumber = componentsName.size()
     def componets1 = componentsName.get(1)
     echo "There are $componetsNumber components in ths manifest"
-/*    echo "The second component is called $componets1"
+    echo "The second component is called $componets1"
     def componets2 = componentsName.get(2)
-    echo "The third component is called $componets2"*/
+    echo "The third component is called $componets2"
     sh """
         pwd
-        ls $WORKSPACE/builds/opensearch
+        ls $WORKSPACE/opensearch-build/jenkins/release-tag
     """
     withCredentials([usernamePassword(credentialsId: "release-tag-test-token", usernameVariable: 'GITHUB_USER', passwordVariable: 'GITHUB_TOKEN')]) {
+        echo "Now doing the tag ******************************"
         for (component in componentsName) {
             echo "The component name is $component"
             def commitID = buildManifestObj.getCommitId(component)
             echo "The commit ID for $component is $commitID"
             def repo = buildManifestObj.getRepo(component)
+            def push_url = "https://$GITHUB_TOKEN@" + repo.minus('https://')
+            def version = args.Version
             echo "The URL for $component is $repo"
-
+            sh """
+                echo "Tagging $component at $commitID ..."
+                mkdir $component
+                cd $component
+                pwd
+                git init
+                git remote add origin $repo
+                git fetch --depth 1 origin $commitID
+                git checkout FETCH_HEAD
+                if [ "$component" == "OpenSearch" ]; then
+                    git tag $version
+                else
+                    git tag $version.0
+                fi
+                git push $push_url --tags
+                cd ..
+            """
         }
-        echo "Now doing the tag ******************************"
+
         def name="myownbuild"
         def commit_id="e19608bc0c17e249e5bab0182df6a5e2a9539f00"
         def ref="fix-cve"
         def repo='https://github.com/zelinh/opensearch-build.git'
         def version = "1.2.3"
         def push_url = "https://$GITHUB_TOKEN@" + repo.minus('https://')
-        sh """
-            echo "Lets dooooo this"
-            echo "Tagging $name at $commit_id ..."
-            mkdir $name
-            cd $name
-            pwd
-            git init
-            git remote add origin $repo
-            git fetch --depth 1 origin $commit_id
-            git checkout FETCH_HEAD
-            git tag $version.0
-            git push $push_url --tags
-            cd ..
-        """
+
     }
 
 }
