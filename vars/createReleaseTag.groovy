37ba0cd84beb99aec268cede5473acabf1570aef
Zelin Hao <zelinhao@amazon.com>
2022-02-25 14:06:27 -0800

    TetstTEST
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/createReleaseTag.groovy
+++ vars/createReleaseTag.groovy
@@ -40,8 +40,14 @@ def call(Map args = [:]) {
                 git fetch --depth 1 origin $commitID
                 git checkout FETCH_HEAD
                 if [ "$component" == "OpenSearch" ]; then
+                    if [ git ls-remote --tags | grep refs/tags/$version]; then
+                        git push -delete $push_url $version
+                    fi
                     git tag $version
                 else
+                    if [ git ls-remote --tags | grep refs/tags/$version.0]; then
+                        git push -delete $push_url $version.0
+                    fi
                     git tag $version.0
                 fi
                 git push $push_url --tags
