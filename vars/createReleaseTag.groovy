c47c797603ea3e26b75a3ae85b2fc858da12fcd9
Zelin Hao <zelinhao@amazon.com>
2022-03-01 16:47:48 -0800

    Test with
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/createReleaseTag.groovy
+++ vars/createReleaseTag.groovy
@@ -40,11 +40,29 @@ def call(Map args = [:]) {
                 git fetch --depth 1 origin $commitID
                 git checkout FETCH_HEAD
                 if [ "$component" == "OpenSearch" ]; then
-                    echo "aaa ****"
+                    if [[ -n \$(git ls-remote --tags $repo $version) ]]; then
+                        tag_id=\$(git ls-remote --tags $repo $version | awk 'NR==1{print \$1}')
+                        if [ tag_id != $commitID ]; then
+                            echo "Tag $version already existed with a different commit ID. Please check this." 
+                            exit 1
+                        else
+                            echo "Tag $version has been created with identical commit ID. Skipping creating new tag for $component."
+                        fi
+                    else
+                        git tag $version
+                    fi
                 else
-                    git ls-remote --tags $repo $version.0 > tags_list
-                    cat tags_list
-                    
+                    if [[ -n \$(git ls-remote --tags $repo $version.0) ]]; then
+                        tag_id=\$(git ls-remote --tags $repo $version.0 | awk 'NR==1{print \$1}')
+                        if [ tag_id != $commitID ]; then
+                            echo "Tag $version already existed with a different commit ID. Please check this." 
+                            exit 1
+                        else
+                            echo "Tag $version.0 has been created with identical commit ID. Skipping creating new tag for $component."
+                        fi
+                    else
+                        git tag $version.0
+                    fi
                 fi
                 git push $push_url --tags
                 cd ..
@@ -70,16 +88,7 @@ def call(Map args = [:]) {
 //        git tag $version
 //        fi
 
-//        if [[ -n tags_list ]]; then
-//        if [[ cat tags_list | awk 'NR==1{print \$1}' != $commitID ]]; then
-//        echo "Tag $version.0 already existed with a different commit ID. Please check this."
-//        exit 1
-//        else
-//        echo "Tag $version.0 has been created with correct commit ID. Skipping creating for $component."
-//        fi
-//        else
-//        git tag $version.0
-//        fi
+
     }
 
 }
