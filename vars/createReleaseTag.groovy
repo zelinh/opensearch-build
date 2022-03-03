cd35f1af7d1e874932afb92c915838058f61d833
Zelin Hao <zelinhao@amazon.com>
2022-03-03 15:28:45 -0800

    Tetst
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/createReleaseTag.groovy
+++ vars/createReleaseTag.groovy
@@ -30,34 +30,10 @@ def call(Map args = [:]) {
             def repo = buildManifestObj.getRepo(component)
             def push_url = "https://$GITHUB_TOKEN@" + repo.minus('https://')
             echo "The URL for $component is $repo"
-            try {
-                sh """
-                echo "Tagging $component at $commitID ..."
-                mkdir $component
-                cd $component
-                pwd
-                git init
-                git remote add origin $repo
-                git fetch --depth 1 origin $commitID
-                git checkout FETCH_HEAD
-                if [ "$component" == "OpenSearch" ]; then
-                    export tag_list=\$(git ls-remote --tags $repo $version)
-                    export tag_id=\$(git ls-remote --tags $repo $version | awk 'NR==1{print \$1}')
-                    git tag $version
-                else
-                    export tag_list=\$(git ls-remote --tags $repo $version.0)
-                    export tag_id=\$(git ls-remote --tags $repo $version.0 | awk 'NR==1{print \$1}')
-                    git tag $version.0
-                fi
-                git push $push_url --tags
-                cd ..
-            """
-            } catch(Exception ex) {
-                echo "$tag_list"
-                echo "$tag_id"
-                echo "Tag $version already existed with a different commit ID. Please check this."
-
+            dir ('$component') {
+                sh 'pwd && ls'
             }
+            sh 'pwd && ls'
 
         }
 
@@ -67,19 +43,42 @@ def call(Map args = [:]) {
         def repo='https://github.com/zelinh/opensearch-build.git'
         //def version = "1.2.3"
         def push_url = "https://$GITHUB_TOKEN@" + repo.minus('https://')
-//        git ls-remote --tags $repo $version > tags_list
-//        cat tags_list
-//        if [[ -n tags_list ]]; then
-//        if [[ cat tags_list | awk 'NR==1{print \$1}' != $commitID ]]; then
-//        echo "Tag $version already existed with a different commit ID. Please check this."
-//        exit 1
-//        else
-//        echo "Tag $version has been created with correct commit ID. Skipping creating for $component."
-//        fi
-//        else
-//        git tag $version
-//        fi
 
+//        sh """
+//                mkdir $component
+//                cd $component
+//                git init
+//                git remote add origin $repo
+//                git fetch --depth 1 origin $commitID
+//                git checkout FETCH_HEAD
+//                if [ "$component" == "OpenSearch" ]; then
+//                    if [[ -n \$(git ls-remote --tags $repo $version) ]]; then
+//                        tag_id=\$(git ls-remote --tags $repo $version | awk 'NR==1{print \$1}')
+//                        if [[ \${tag_id} != $commitID ]]; then
+//                            echo "Tag $version already existed with a different commit ID. Please check this."
+//                            exit 1
+//                        else
+//                            echo "Tag $version has been created with identical commit ID. Skipping creating new tag for $component."
+//                        fi
+//                    else
+//                        git tag $version
+//                    fi
+//                else
+//                    if [[ -n \$(git ls-remote --tags $repo $version.0) ]]; then
+//                        tag_id=\$(git ls-remote --tags $repo $version.0 | awk 'NR==1{print \$1}')
+//                        if [[ \${tag_id} != $commitID ]]; then
+//                            echo "Tag $version.0 already existed with a different commit ID. Please check this."
+//                            exit 1
+//                        else
+//                            echo "Tag $version.0 has been created with identical commit ID. Skipping creating new tag for $component."
+//                        fi
+//                    else
+//                        git tag $version.0
+//                    fi
+//                fi
+//                git push $push_url --tags
+//                cd ..
+//            """
 
     }
 
