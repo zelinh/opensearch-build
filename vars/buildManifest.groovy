d9a88ad14b5608a39ebc3da0df99ea12b9804f89
Zelin Hao <zelinhao@amazon.com>
2022-03-21 18:23:39 -0700

    Add rpm build
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/buildManifest.groovy
+++ vars/buildManifest.groovy
@@ -4,6 +4,7 @@ void call(Map args = [:]) {
         args.inputManifest ?: "manifests/${INPUT_MANIFEST}",
         args.platform ? "-p ${args.platform}" : null,
         args.architecture ? "-a ${args.architecture}" : null,
+        args.distribution ? "-d ${args.distribution}" : null,
         args.snapshot ? '--snapshot' : null,
         args.lock ? '--lock' : null
     ] - null).join(' '))
