30b11316691bbd3e04ff66b822cf78e774d35f95
Zelin Hao <zelinhao@amazon.com>
2022-08-03 17:59:06 -0700

    Test
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/yamlUpdate.groovy
+++ vars/yamlUpdate.groovy
@@ -1,5 +1,5 @@
 def call(Map args = [:]) {
-    lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
+    def lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
 
     echo ("Im in the groovy**************")
     def inputManifest = readYaml(file: args.inputManifest)
