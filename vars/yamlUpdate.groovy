fbd2081fbe9c15e7404dd017c2bbe982bf304988
Zelin Hao <zelinhao@amazon.com>
2022-08-05 12:05:52 -0700

    Test
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/yamlUpdate.groovy
+++ vars/yamlUpdate.groovy
@@ -5,25 +5,31 @@ def call(Map args = [:]) {
     def inputManifest = readYaml(file: args.inputManifest)
     def outputFile = args.outputFile
     def components = args.componentName
+    def componentsList = []
 
     if (components != null) {
         componentsList = inputManifest.components
-    }
-    if (args.stage == "START") {
-        inputManifest.build.status = "IN_PROGRESS"
+    } else {
         inputManifest.components.each { component ->
-            component.status = "NOT_START"
-        }
-    } else if (args.stage == "IN_PROGRESS") {
-        inputManifest.build.each { component ->
-            component.status = "IN_PROGRESS"
-        }
-    } else if (args.stage == "COMPLETE") {
-        inputManifest.build.status = "COMPLETED"
-        inputManifest.components.each { component ->
-            component.status = "COMPLETED"
+            componentsList.add(component.name)
         }
     }
-    writeYaml(file: outputFile, data: inputManifest, overwrite: true)
-    sh("yq -i $outputFile")
+    echo (componentsList)
+//    if (args.stage == "START") {
+//        inputManifest.build.status = "IN_PROGRESS"
+//        inputManifest.components.each { component ->
+//            component.status = "NOT_START"
+//        }
+//    } else if (args.stage == "IN_PROGRESS") {
+//        inputManifest.build.each { component ->
+//            component.status = "IN_PROGRESS"
+//        }
+//    } else if (args.stage == "COMPLETE") {
+//        inputManifest.build.status = "COMPLETED"
+//        inputManifest.components.each { component ->
+//            component.status = "COMPLETED"
+//        }
+//    }
+//    writeYaml(file: outputFile, data: inputManifest, overwrite: true)
+//    sh("yq -i $outputFile") //reformat the yaml
 }
