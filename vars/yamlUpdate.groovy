c8aed1850bec0cadc05f64ac94b0c35d6bb29031
Zelin Hao <zelinhao@amazon.com>
2022-08-05 14:56:14 -0700

    Test
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/yamlUpdate.groovy
+++ vars/yamlUpdate.groovy
@@ -7,7 +7,7 @@ def call(Map args = [:]) {
     def components = args.componentName
     def componentsList = []
     echo("Components is $components")
-    if (components.isEmpty()) {
+    if (!components.isEmpty()) {
         echo ("Components is not null")
         componentsList = inputManifest.components
     } else {
