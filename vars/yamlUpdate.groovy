6522794808918d07f627ca7893c838c09853a8f7
Zelin Hao <zelinhao@amazon.com>
2022-08-10 15:56:35 -0700

    Add dist
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/yamlUpdate.groovy
+++ vars/yamlUpdate.groovy
@@ -54,7 +54,8 @@ def call(Map args = [:]) {
     }
     else if (args.stage == "COMPLETE") {
         inputManifest.build.status = status
-        inputManifest.results.duration = args.duration
+        inputManifest.results.duration = currentBuild.duration
+        inputManifest.results.startTime = currentBuild.startTimeInMillis
     }
     writeYaml(file: outputFile, data: inputManifest, overwrite: true)
     sh("yq -i $outputFile") //reformat the yaml
