d4a682afd8ce9f8e646c891934037101e28e9bba
Zelin Hao <zelinhao@amazon.com>
2022-08-17 18:03:20 -0700

    Test
    Signed-off-by: Zelin Hao <zelinhao@amazon.com>

--- vars/yamlUpdate.groovy
+++ vars/yamlUpdate.groovy
@@ -21,25 +21,19 @@ def call(Map args = [:]) {
         }
     }
     echo (componentsList.toString())
+
     if (args.stage == "START") {
-        echo("we are on the start stage.")
+        echo("Initiate the buildInfo yaml file.")
         inputManifest.build.status = "IN_PROGRESS"
         inputManifest.build.number = "${BUILD_NUMBER}"
         inputManifest.results = [:]
-        echo("status is $status")
-        inputManifest.results.integ_test = status
-        inputManifest.results.bwc_test = status
-        inputManifest.results.x64_tar = status
-        inputManifest.results.arm64_tar = status
-        inputManifest.results.x64_rpm = status
-        inputManifest.results.arm64_rpm = status
         inputManifest.components.each { component ->
             if (componentsList.contains(component.name)) {
                 // Convert ref from branch to commit
-                dir (component.name) {
-                    checkout([$class: 'GitSCM', branches: [[name: component.ref]],
+                dir(component.name) {
+                    checkout([$class           : 'GitSCM', branches: [[name: component.ref]],
                               userRemoteConfigs: [[url: component.repository]]])
-                    def commitID = sh (
+                    def commitID = sh(
                             script: "git rev-parse HEAD",
                             returnStdout: true
                     ).trim()
@@ -47,25 +41,61 @@ def call(Map args = [:]) {
                 }
             }
         }
-    }
-        // x64_tar; x64_rpm; arm_tar; arm_rpm
-    else if (args.stage == "x64_tar" || args.stage == "x64_rpm" || args.stage == "arm64_tar" || args.stage == "arm64_rpm") {
-        stageField = args.stage
-        echo("stage is $stageField")
-        echo("status is $status")
-        inputManifest.results.("$stageField".toString()) = "$status"
-    }
-    else if (args.stage == "integ_test" || args.stage == "bwc_test") {
-        stageField = args.stage
-        echo("stage is $stageField")
-        echo("status is $status")
-        inputManifest.results.("$stageField".toString()) = "$status"
-    }
-    else if (args.stage == "COMPLETE") {
+    } else if (args.stage == "COMPLETE") {
         inputManifest.build.status = status
+    } else {
+        stageField = args.stage
+        echo("stage is $stageField")
+        echo("status is $status")
+        inputManifest.results.("$stageField".toString()) = "$status"
         inputManifest.results.duration = currentBuild.duration
         inputManifest.results.startTimestamp = currentBuild.startTimeInMillis
     }
+//    if (args.stage == "START") {
+//        echo("we are on the start stage.")
+//        inputManifest.build.status = "IN_PROGRESS"
+//        inputManifest.build.number = "${BUILD_NUMBER}"
+//        inputManifest.results = [:]
+//        echo("status is $status")
+//        inputManifest.results.integ_test = status
+//        inputManifest.results.bwc_test = status
+//        inputManifest.results.x64_tar = status
+//        inputManifest.results.arm64_tar = status
+//        inputManifest.results.x64_rpm = status
+//        inputManifest.results.arm64_rpm = status
+//        inputManifest.components.each { component ->
+//            if (componentsList.contains(component.name)) {
+//                // Convert ref from branch to commit
+//                dir (component.name) {
+//                    checkout([$class: 'GitSCM', branches: [[name: component.ref]],
+//                              userRemoteConfigs: [[url: component.repository]]])
+//                    def commitID = sh (
+//                            script: "git rev-parse HEAD",
+//                            returnStdout: true
+//                    ).trim()
+//                    component.ref = commitID
+//                }
+//            }
+//        }
+//    }
+//        // x64_tar; x64_rpm; arm_tar; arm_rpm
+//    else if (args.stage == "x64_tar" || args.stage == "x64_rpm" || args.stage == "arm64_tar" || args.stage == "arm64_rpm") {
+//        stageField = args.stage
+//        echo("stage is $stageField")
+//        echo("status is $status")
+//        inputManifest.results.("$stageField".toString()) = "$status"
+//    }
+//    else if (args.stage == "integ_test" || args.stage == "bwc_test") {
+//        stageField = args.stage
+//        echo("stage is $stageField")
+//        echo("status is $status")
+//        inputManifest.results.("$stageField".toString()) = "$status"
+//    }
+//    else if (args.stage == "COMPLETE") {
+//        inputManifest.build.status = status
+//        inputManifest.results.duration = currentBuild.duration
+//        inputManifest.results.startTimestamp = currentBuild.startTimeInMillis
+//    }
     writeYaml(file: outputFile, data: inputManifest, overwrite: true)
 //    sh("yq -i $outputFile") //reformat the yaml
     sh ("cat $outputFile")
